5ca57a3ed585ab69305c8e6240bbc8c06382338a
Allen <aleonard@basledi.com>
2018-05-22 21:31:20 -0500

    Added single record query.

---
+++ cmd/query/main.go
@@ -0,0 +1,26 @@
+//Query for a record, see JSON.
+package main
+
+import (
+	"log"
+
+	"github.com/salsalabs/godig"
+	kingpin "gopkg.in/alecthomas/kingpin.v2"
+)
+
+func main() {
+	cpath := kingpin.Flag("credentials", "YAML file containing credentials for Salsa Classic API").PlaceHolder("FILENAME").Required().String()
+	table := kingpin.Flag("table", "table name ([supporter], donation, groups, etc.)").PlaceHolder("TABLE").Default("supporter").String()
+	key := kingpin.Flag("key", "primary key").PlaceHolder("KEY").Required().String()
+	kingpin.Parse()
+	api, err := godig.YAMLAuth(*cpath)
+	if err != nil {
+		log.Fatalf("Authentication error %v\n", err)
+	}
+	t := api.NewTable(*table)
+	b, err := t.OneRaw(*key)
+	if err != nil {
+		log.Fatalf("Query error %v\n", err)
+	}
+	log.Printf("%v\n", string(b))
+}
