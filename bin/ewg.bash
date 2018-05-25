26cc54e0829233100f19717d2c2e449ac91bec6b
Allen Leonard (Salsalabs) <aleonard@salsalabs.com>
2018-05-25 21:40:48 +0000

    Supplied missing &condition= clauses.

--- bin/ewg.bash
+++ bin/ewg.bash
@@ -1,8 +1,8 @@
 #! /bin/bash
-rm addressfixer*.log
+rm -f addressfixer*.log
 go run cmd/addressfixer/app/app.go \
 --credentials credentials/ewg.yaml \
---criteria 'Email%20IS%20NOT%20EMPTY&Receive_Email>0&State%20IS%20EMPTY' \
+--criteria 'Email%20IS%20NOT%20EMPTY&condition=Email%20LIKE&#37;@&#37;.&#37;&condition=Receive_Email>0&condition=State%20IS%20EMPTY' \
 --fixer-count 25 \
 --reader-count 25 \
 --file-log
