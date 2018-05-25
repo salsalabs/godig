81708b48fd89c1255a5326e12fcc3278cfd824bb
Allen Leonard (Salsalabs) <aleonard@salsalabs.com>
2018-05-25 23:01:40 +0000

    Settled on the content of the conditions.

--- bin/ewg.bash
+++ bin/ewg.bash
@@ -2,7 +2,7 @@
 rm -f addressfixer*.log
 go run cmd/addressfixer/app/app.go \
 --credentials credentials/ewg.yaml \
---criteria 'Email%20IS%20NOT%20EMPTY%26condition%3DEmail%20LIKE%20%25%40%25.%25%26condition%3DReceive_Email%20IN%201%2C2%2C3%2C10%2C15%26condition%3DState%20IS%20EMPTY' \
+--criteria 'Email%20IS%20NOT%20EMPTY&condition%3DEmail%20LIKE%20&apos;@&apos;.&apos;%40%25.%25&condition%3DReceive_Email>0&condition%3DState%20IS%20EMPTY' \
 --fixer-count 25 \
 --reader-count 25 \
 --file-log
