535d515fe086c26eb873c1bcb467ab5b8451c0a5
Allen Leonard (Salsalabs) <aleonard@salsalabs.com>
2018-05-25 22:48:47 +0000

    End of data is one empty record.

--- cmd/addressfixer/reader.go
+++ cmd/addressfixer/reader.go
@@ -9,7 +9,6 @@ import (
 //ReadAll implements Reader.  Reads all supporters for a criteria
 //then passes arrays of supporter JSON downstream.
 func ReadAll(t *godig.Table, crit string, c1 chan []Supporter, id int, m *sync.Mutex, c2 chan int32, done chan bool) {
-	count := 500
 	ok := true
 
 	for ok {
@@ -21,11 +20,16 @@ func ReadAll(t *godig.Table, crit string, c1 chan []Supporter, id int, m *sync.M
 		}
 		log.Printf("ReadAll: id %2d popped %7d", id, offset)
 		var a []Supporter
+		count := 500
 		err := t.Many(int(offset), count, crit, &a)
 		if err != nil {
 			log.Fatalf("ReadAll: id %2d offset %6d %v\n", id, offset, err)
 		}
+		// Empty read returns [{  }].  Interesting, no?
 		count = len(a)
+		if count == 1 && len(a[0].Key) == 0 {
+			count = 0
+		}
 		if count == 0 {
 			log.Printf("ReadAll: id %2d offset %7d, done\n", id, offset)
 			done <- true
