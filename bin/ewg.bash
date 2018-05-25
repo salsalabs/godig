5d5c29d4224ee08bf6300bcdecf9503e6d2d86bd
Allen Leonard (Salsalabs) <aleonard@salsalabs.com>
2018-05-25 16:59:50 +0000

    Fixed typos and upped the number of units.

--- bin/ewg.bash
+++ bin/ewg.bash
@@ -1,8 +1,8 @@
 #! /bin/bash
-rm addressfixer*-
+rm addressfixer*.log
 go run cmd/addressfixer/app/app.go \
 --credentials credentials/ewg.yaml \
 --criteria 'Email%20IS%20NOT%20EMPTY&Receive_Email>0&State%20IS%20EMPTY' \
---fixer-count 10 \
---reader-count 10 \
+--fixer-count 25 \
+--reader-count 25 \
 --file-log
