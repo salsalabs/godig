50450a6c9ec126e98351639b02146f93c7188a1a
Allen <aleonard@basledi.com>
2018-05-26 00:02:10 -0500

    New parameters that get just what we need.

--- bin/ewg.bash
+++ bin/ewg.bash
@@ -2,7 +2,7 @@
 rm -f addressfixer*.log
 go run cmd/addressfixer/app/app.go \
 --credentials credentials/ewg.yaml \
---criteria 'Email%20IS%20NOT%20EMPTY&condition%3DEmail%20LIKE%20&apos;@&apos;.&apos;%40%25.%25&condition%3DReceive_Email>0&condition%3DState%20IS%20EMPTY' \
+--criteria 'Email%20IS%20NOT%20EMPTY&condition=Receive_Email>0&condition=State%20IS%20EMPTY&include=supporter_KEY,Email,City,State,Zip,Country,Receive_Email' \
 --fixer-count 25 \
 --reader-count 25 \
 --file-log
