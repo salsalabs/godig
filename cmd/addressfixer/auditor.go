672d6c38b2a96b00697031918f545dbeab596238
Allen Leonard (Salsalabs) <aleonard@salsalabs.com>
2018-05-26 23:00:15 +0000

    Clarified change reason.

--- cmd/addressfixer/auditor.go
+++ cmd/addressfixer/auditor.go
@@ -10,7 +10,7 @@ func Audit(c chan Mod) {
 		if len(a.Old) == 0 {
 			a.Old = "(empty)"
 		}
-		log.Printf("Audit:   Key: %-8s Field: %-10s  New: %-10s  Old: %-20s Reason: %s\n", a.Key, a.Field, a.New, a.Old, a.Reason)
+		log.Printf("Audit:   Key: %-8s Field: %-10s Old: %-20s New: %-40s Reason: %s\n", a.Key, a.Field, a.Old, a.New, a.Reason)
 		count = count + 1
 	}
 	log.Printf("Audit:   done, %d records\n", count)
