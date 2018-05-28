26938243ab829c839965f6e80bfe24a47b2af5bd
Allen Leonard (Salsalabs) <aleonard@salsalabs.com>
2018-05-28 19:38:56 +0000

    Removed unwanted directories.

--- bin/aauw_group_overlay.bash
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-go run cmd/group/overlap/main.go --credentials credentials/aauw.json --start 2017-11-01 --end 2018-01-01
--- bin/ewg.bash
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-rm -f addressfixer*.log
-go run cmd/addressfixer/app/app.go \
---credentials credentials/ewg.yaml \
---criteria 'Email%20IS%20NOT%20EMPTY&condition=Receive_Email>0&condition=State%20IS%20EMPTY&include=supporter_KEY,Email,City,State,Zip,Country,Receive_Email' \
---fixer-count 25 \
---reader-count 25 \
---file-log
