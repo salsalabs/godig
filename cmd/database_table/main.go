f9783886ed188668ec6ab3a7b7512e2f748346db
Allen Leonard (Salsalabs) <aleonard@salsalabs.com>
2018-08-04 18:34:44 +0000

    Utility to show how to read database_table.

---
+++ cmd/database_table/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/salsalabs/godig"
+	kingpin "gopkg.in/alecthomas/kingpin.v2"
+)
+
+func main() {
+	var (
+		login = kingpin.Flag("login", "YAML file with login credentials").Required().String()
+		table = kingpin.Flag("table", "Get database_table record for this table").Default("supporter").String()
+	)
+	kingpin.Parse()
+	api, err := (godig.YAMLAuth(*login))
+	if err != nil {
+		log.Fatalf("%v\n", err)
+	}
+
+	dt := api.NewTable("database_table")
+	var a []godig.DatabaseTable
+	crit := fmt.Sprintf("table_name=%v\n", *table)
+	err = dt.ManyBig(int32(0), 500, crit, &a)
+	if err != nil {
+		panic(err)
+	}
+	for _, d := range a {
+		fmt.Printf("%+v\n", d)
+	}
+}
