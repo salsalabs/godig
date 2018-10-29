1bba4aa78fc43178785972216676abcd801ec3f2
Allen Leonard <aleonard@salsalabs.com>
2018-10-29 18:43:17 -0500

    Added push to write offsets to a channel.

--- cmd/blast-content/main.go
+++ cmd/blast-content/main.go
@@ -3,6 +3,9 @@ package main
 
 import (
 	"log"
+	"strconv"
+	"sync"
+	"time"
 
 	"github.com/salsalabs/godig"
 	kingpin "gopkg.in/alecthomas/kingpin.v2"
@@ -17,6 +20,28 @@ type EmailBlast struct {
 	TextContent   string `json:"Text_Content"`
 }
 
+//Push counts the number of records to read, then pushes
+//read offsets into a channel.
+func Push(t *godig.Table, c chan int32) error {
+	log.Println("Push start")
+	crit := "Stage=Complete"
+	x, err := t.Count(crit)
+	if err != nil {
+		return err
+	}
+	y, err := strconv.ParseInt(x, 10, 32)
+	if err != nil {
+		return err
+	}
+	log.Printf("Push %d records\n", y)
+	max := int32(y) + 500
+	for i := int32(0); i < max; i += 500 {
+		c <- i
+	}
+	close(c)
+	log.Println("Push done")
+	return nil
+}
 func main() {
 	login := kingpin.Flag("login", "YAML file containing credentials for Salsa Classic API").PlaceHolder("FILENAME").Required().String()
 	kingpin.Parse()
@@ -24,4 +49,22 @@ func main() {
 	if err != nil {
 		log.Fatalf("Authentication error %v\n", err)
 	}
+	t := api.NewTable("email_blast")
+	c := make(chan int32, 500)
+	var w sync.WaitGroup
+	log.Println("Main start")
+
+	go (func(t *godig.Table, c chan int32, w *sync.WaitGroup) {
+		w.Add(1)
+		err := Push(t, c)
+		if err != nil {
+			panic(err)
+		}
+		w.Done()
+	})(&t, c, &w)
+
+	//Settle time then wait for things to end.
+	time.Sleep(10000)
+	w.Wait()
+	log.Println("Main done")
 }
