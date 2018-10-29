0bcf60b2e3cbc7464dcb8e045079106d183892ef
Allen Leonard <aleonard@salsalabs.com>
2018-10-29 18:28:44 -0500

    Defined blast content.

--- cmd/blast-content/main.go
+++ cmd/blast-content/main.go
@@ -8,8 +8,17 @@ import (
 	kingpin "gopkg.in/alecthomas/kingpin.v2"
 )
 
+//EmailBlast is the content that this app will read from Salsa Classic.
+type EmailBlast struct {
+	EmailBlastKey int32  `json:"email_blast_KEY"`
+	DateRequested string `json:"Date_Requested"`
+	Subject       string `json:"Subject"`
+	HTMLContent   string `json:"HTML_Content"`
+	TextContent   string `json:"Text_Content"`
+}
+
 func main() {
-	cpath := kingpin.Flag("login", "YAML file containing credentials for Salsa Classic API").PlaceHolder("FILENAME").Required().String()
+	login := kingpin.Flag("login", "YAML file containing credentials for Salsa Classic API").PlaceHolder("FILENAME").Required().String()
 	kingpin.Parse()
 	api, err := godig.YAMLAuth(*login)
 	if err != nil {
