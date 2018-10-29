1bff39229d4f63b5232212bd81bb9b8b282e4208
Allen Leonard <aleonard@salsalabs.com>
2018-10-29 18:22:29 -0500

    Added conversion to Engage date.

--- main.go
+++ main.go
@@ -1,10 +1,20 @@
 package godig
 
 import (
+	"log"
 	"net/http"
+	"strings"
+	"time"
 
 	"github.com/tidwall/gjson"
 )
+
+//ParseFmt is used to parse a Salsa database mesasge.  NOote that the only
+//way to do that is to remove the hour offset before parsing.  See `date()`.
+const ParseFmt = "Mon Jan 2 2006 15:04:05 (MST)"
+
+//LayoutFmt is used to format a time so that Engage will recognize it.
+const LayoutFmt = "2006-Jan-02T15:04:05"
 
 //API hold the data that we need to do Salsa API calls.  That includes
 //the cookies from authentication.
@@ -104,4 +114,23 @@ func (a *API) SupporterGroups() Table {
 //Publish is a shortcut for creating a publish Table.
 func (a *API) Publish() Table {
 	return a.NewTable("publish")
+}
+
+//EngageDate parses converts a string containing a MySQL date to
+//another string containing an Engage date.
+func EngageDate(s string) string {
+	// Date_Created, Transaction_Date, etc.  Convert dates from MySQL to Engage.
+	p := strings.Split(s, " ")
+	if len(p) >= 7 {
+		//Pull out the timezone.
+		p = append(p[0:5], p[6])
+		x := strings.Join(p, " ")
+		t, err := time.Parse(ParseFmt, x)
+		if err != nil {
+			log.Printf("Warning: parsing %v returned %v\n", s, err)
+		} else {
+			s = t.Format(LayoutFmt)
+		}
+	}
+	return s
 }
