39444ad7b27d86c29915f298d6a5149a9d063cb7
Allen Leonard <aleonard@salsalabs.com>
2019-06-18 12:07:34 -0500

    Larger page format appears to work better.

--- cmd/list_events/hn.js
+++ cmd/list_events/hn.js
@@ -4,7 +4,7 @@ const puppeteer = require('puppeteer');
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(process.argv[2], {waitUntil: 'networkidle2'});
-  await page.pdf({path: process.argv[3], format: 'Letter'});
+  await page.pdf({path: process.argv[3], format: 'Tabloid'});
 
   await browser.close();
 })();
