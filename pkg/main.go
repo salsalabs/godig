aae0391ba7da75cd74afdf652fb9ea87aa50057f
Allen Leonard <aleonard@salsalabs.com>
2019-11-14 10:00:28 -0600

    Fixed date parse format for Salsa timestamps.

--- pkg/main.go
+++ pkg/main.go
@@ -9,9 +9,11 @@ import (
 	"github.com/tidwall/gjson"
 )
 
-//ParseFormat is used to parse a Salsa database mesasge.  NOote that the only
-//way to do that is to remove the hour offset before parsing.  See `date()`.
-const ParseFormat = "Mon Jan 2 2006 15:04:05 (MST)"
+//ClassicDateFormat is used to parse a Salsa timestamp coming from the database.
+const ClassicDateFormat = "Mon Jan 2 2006 15:04:05 GMT-0700 (MST)"
+
+//EngageDateFormat is used to format a date for Engage.
+const EngageDateFormat = "2006-01-02T15:04:05.000Z"
 
 //DateFormat is used to format a time so that Engage will recognize it.
 const DateFormat = "2006-01-02"
@@ -38,6 +40,14 @@ type Table struct {
 type AuthStatus struct {
 	Status  string
 	Message string
+}
+
+//Results is returned by API calls.
+type Results struct {
+	Object   string   `json:"object"`
+	Key      string   `json:"key"`
+	Result   string   `json:"result"`
+	Messages []string `json:"messages"`
 }
 
 //CredData contains the info that we need to get into the API.
@@ -110,6 +120,11 @@ func (a *API) GroupsSupporters() Table {
 	return a.NewTable("groups(groups_KEY)supporter_groups(supporter_KEY)supporter")
 }
 
+//Org is a shortcut for creating an organization Table object.
+func (a *API) Org() Table {
+	return a.NewTable("organization")
+}
+
 //Supporter is a shortcut for creating a supporter Table object.
 func (a *API) Supporter() Table {
 	return a.NewTable("supporter")
@@ -132,21 +147,27 @@ func (a *API) Publish() Table {
 	return a.NewTable("publish")
 }
 
+//ShortDate accepts a time and outputs it as YYYY-mm-dd.
+func ShortDate(s string) string {
+	s = strings.TrimSpace(s)
+	if len(s) == 0 {
+		return s
+	}
+	t, err := time.Parse(ClassicDateFormat, s)
+	if err != nil {
+		log.Fatalf("%v: %v\n", err, s)
+	}
+	return t.Format(DateFormat)
+}
+
 //EngageDate parses converts a string containing a MySQL date to
 //another string containing an Engage date.
 func EngageDate(s string) string {
-	// Date_Created, Transaction_Date, etc.  Convert dates from MySQL to Engage.
-	p := strings.Split(s, " ")
-	if len(p) >= 7 {
-		//Pull out the timezone.
-		p = append(p[0:5], p[6])
-		x := strings.Join(p, " ")
-		t, err := time.Parse(ParseFormat, x)
-		if err != nil {
-			log.Printf("Warning: parsing %v returned %v\n", s, err)
-		} else {
-			s = t.Format(DateFormat)
-		}
+	t, err := time.Parse(ClassicDateFormat, s)
+	if err != nil {
+		log.Printf("Warning: parsing %v returned %v\n", s, err)
+	} else {
+		s = t.Format(EngageDateFormat)
 	}
 	return s
 }
@@ -160,7 +181,7 @@ func EngageTimestamp(s string) string {
 		//Pull out the timezone.
 		p = append(p[0:5], p[6])
 		x := strings.Join(p, " ")
-		t, err := time.Parse(ParseFormat, x)
+		t, err := time.Parse(ClassicDateFormat, x)
 		if err != nil {
 			log.Printf("Warning: parsing %v returned %v\n", s, err)
 		} else {
@@ -168,4 +189,88 @@ func EngageTimestamp(s string) string {
 		}
 	}
 	return s
+}
+
+//Organization describes a record for an Salsa Classic client.
+//Be aware that the bulk of the non-identity and non-status fields
+//have been deprecated.
+type Organization struct {
+	OrganizationKEY                            string `json:"organization_KEY"`
+	RootKEY                                    string `json:"root_KEY,omitempty"`
+	ParentKEY                                  string `json:"parent_KEY,omitempty"`
+	PartnerKEY                                 string `json:"partner_KEY,omitempty"`
+	LastModified                               string `json:"Last_Modified,omitempty"`
+	DateCreated                                string `json:"Date_Created,omitempty"`
+	PRIVATEDateCreated                         string `json:"PRIVATE_Date_Created,omitempty"`
+	Name                                       string `json:"Name"`
+	Type                                       string `json:"Type"`
+	Status                                     string `json:"Status"`
+	READONLYShortName                          string `json:"READONLY_Short_Name,omitempty"`
+	Description                                string `json:"Description,omitempty"`
+	OrganizationHomepage                       string `json:"Organization_Homepage,omitempty"`
+	NewsletterOrListserveName                  string `json:"Newsletter_or_Listserve_Name,omitempty"`
+	CustomHeaderHTML                           string `json:"Custom_Header_HTML,omitempty"`
+	CustomFooterHTML                           string `json:"Custom_Footer_HTML,omitempty"`
+	PrintHeader                                string `json:"Print_Header,omitempty"`
+	PrintFooter                                string `json:"Print_Footer,omitempty"`
+	BaseURL                                    string `json:"Base_URL"`
+	SecureURL                                  string `json:"Secure_URL"`
+	Street                                     string `json:"Street,omitempty"`
+	Street2                                    string `json:"Street_2,omitempty"`
+	City                                       string `json:"City,omitempty"`
+	State                                      string `json:"State,omitempty"`
+	Zip                                        string `json:"Zip,omitempty"`
+	PRIVATEZipPlus4                            string `json:"PRIVATE_Zip_Plus_4,omitempty"`
+	MailServer                                 string `json:"mail_server,omitempty"`
+	MailUser                                   string `json:"mail_user,omitempty"`
+	MailPass                                   string `json:"mail_pass,omitempty"`
+	MailStatus                                 string `json:"Mail_Status,omitempty"`
+	PromotionalCode                            string `json:"Promotional_Code,omitempty"`
+	Interests                                  string `json:"Interests,omitempty"`
+	Note                                       string `json:"Note,omitempty"`
+	GlobalMailHTMLFooter                       string `json:"Global_Mail_HTML_Footer,omitempty"`
+	GlobalMailTextFooter                       string `json:"Global_Mail_Text_Footer,omitempty"`
+	LinkTrackURL                               string `json:"Link_Track_URL,omitempty"`
+	OpenTrackURL                               string `json:"Open_Track_URL,omitempty"`
+	SalsifiedBOOLVALUE                         string `json:"salsified_BOOLVALUE,omitempty"`
+	Salsified                                  bool   `json:"salsified,omitempty"`
+	StatusLastModified                         string `json:"Status_Last_Modified,omitempty"`
+	DateTrialStarted                           string `json:"Date_Trial_Started,omitempty"`
+	ContractDate                               string `json:"Contract_Date,omitempty"`
+	ToolsInContract                            string `json:"Tools_In_Contract,omitempty"`
+	ClosedDate                                 string `json:"Closed_Date,omitempty"`
+	ClosedReason                               string `json:"Closed_Reason,omitempty"`
+	DefaultEmailAddress                        string `json:"default_email_address,omitempty"`
+	DefaultMerchantAccountKEY                  string `json:"default_merchant_account_KEY,omitempty"`
+	MovedBOOLVALUE                             string `json:"moved_BOOLVALUE,omitempty"`
+	Moved                                      bool   `json:"moved,omitempty"`
+	BlastNotificationEmail                     string `json:"Blast_Notification_Email,omitempty"`
+	Country                                    string `json:"Country,omitempty"`
+	ListSize                                   string `json:"List_Size,omitempty"`
+	Usages                                     string `json:"Usages,omitempty"`
+	HearAboutUs                                string `json:"hear_about_us,omitempty"`
+	Tier                                       string `json:"Tier,omitempty"`
+	TaxStatus                                  string `json:"Tax_Status,omitempty"`
+	StaffContact                               string `json:"Staff_Contact,omitempty"`
+	LanguageCode                               string `json:"language_code,omitempty"`
+	DisableTokenAuthenticationBOOLVALUE        string `json:"Disable_Token_Authentication_BOOLVALUE,omitempty"`
+	DisableTokenAuthentication                 bool   `json:"Disable_Token_Authentication,omitempty"`
+	EnforcePackagePermissions                  string `json:"Enforce_Package_Permissions,omitempty"`
+	RecommendedMailServer                      string `json:"recommended_mail_server,omitempty"`
+	OverrideMailServerBOOLVALUE                string `json:"override_mail_server_BOOLVALUE,omitempty"`
+	OverrideMailServer                         bool   `json:"override_mail_server,omitempty"`
+	EmailBlastBrandingOptOutBOOLVALUE          string `json:"Email_Blast_Branding_Opt_Out_BOOLVALUE,omitempty"`
+	EmailBlastBrandingOptOut                   bool   `json:"Email_Blast_Branding_Opt_Out,omitempty"`
+	ExternalClientID                           string `json:"external_client_id,omitempty"`
+	RecommendEmailBlastBrandingOptOutBOOLVALUE string `json:"Recommend_Email_Blast_Branding_Opt_Out_BOOLVALUE,omitempty"`
+	RecommendEmailBlastBrandingOptOut          bool   `json:"Recommend_Email_Blast_Branding_Opt_Out,omitempty"`
+	OrganizationID                             string `json:"organization_id,omitempty"`
+	WebsiteForcedBrandingRequiredBOOLVALUE     string `json:"Website_Forced_Branding_Required_BOOLVALUE,omitempty"`
+	WebsiteForcedBrandingRequired              bool   `json:"Website_Forced_Branding_Required,omitempty"`
+	AuthenticateEmailsBOOLVALUE                string `json:"Authenticate_Emails_BOOLVALUE,omitempty"`
+	AuthenticateEmails                         bool   `json:"Authenticate_Emails,omitempty"`
+	EnforceAutodedupeOnEmailSendBOOLVALUE      string `json:"enforce_autodedupe_on_email_send_BOOLVALUE,omitempty"`
+	EnforceAutodedupeOnEmailSend               bool   `json:"enforce_autodedupe_on_email_send,omitempty"`
+	EnforceHTTPSBOOLVALUE                      string `json:"enforce_https_BOOLVALUE,omitempty"`
+	EnforceHTTPS                               bool   `json:"enforce_https,omitempty"`
 }
