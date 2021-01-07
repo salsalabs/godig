6c47e682de8d98d8d7a18995323e2f245ac9fec3
Allen Leonard <aleonard@salsalabs.com>
2021-01-07 14:48:24 -0600

    Added/fixed comments.

--- cmd/delete_donations/main.go
+++ cmd/delete_donations/main.go
@@ -24,6 +24,9 @@ const (
 	SupporterFlag = "d"
 )
 
+//drive reads donation records that match "criteria" from Salsa Classic.
+//Writes the donation key to "dc" and the supporter key to "sc".  Closes
+//both after all matching donations are read.
 func drive(t *godig.Table, criteria string, dc, sc chan string) {
 	log.Printf("drive: start\n")
 	count := 500
@@ -50,6 +53,9 @@ func drive(t *godig.Table, criteria string, dc, sc chan string) {
 	log.Printf("drive: end %d records\n", total)
 }
 
+//whack accepts primary keys from a channel, then uses the table to delete
+//the matching records.  Displays delete results for every record.  Sends
+//a message on the "done" channel when the keys channel is empty.
 func whack(i int, f string, t *godig.Table, c chan string, done chan bool) {
 	log.Printf("whack-%s-%02d: start\n", f, i)
 	for true {
@@ -71,20 +77,25 @@ func whack(i int, f string, t *godig.Table, c chan string, done chan bool) {
 	log.Printf("whack-%s-%02d: end\n", f, i)
 }
 
+//watch waits for a number of messages on the "done" queue.  Returns after
+//that.  This function makes sure that all channel readers are done before
+//the app terminates.
 func watch(x int, done chan bool) {
 	log.Println("watch: start")
 	for x > 0 {
-		log.Printf("watch: waiting for %d\n", x)
+		log.Printf("watch: waiting for %d task(s)\n", x)
 		_, _ = <-done
 		x--
 	}
 	log.Println("watch: end")
 }
 
+//main accepts command line arguments then deletes donations that match the
+//criteria.
 func main() {
 	cpath := kingpin.Flag("login", "YAML file containing login credentials for Salsa Classic API").PlaceHolder("FILENAME").Required().String()
 	sdate := kingpin.Flag("start-date", "First last modified date as YYYY-MM-YY").Default("2021-01-01").String()
-	edate := kingpin.Flag("end-date", "Day after last last modified date as YYYY-MM-dd").Default("2021-02-01").String()
+	edate := kingpin.Flag("end-date", "Day after last modified date as YYYY-MM-dd").Default("2021-02-01").String()
 	verbose := kingpin.Flag("verbose", "Lots and *lots* of debug noise.  Not recommended...").Bool()
 	kingpin.Parse()
 	if *edate <= *sdate {
@@ -113,7 +124,7 @@ func main() {
 		})(i+1, &wg, &donation, dc, done)
 	}
 
-	// Start donation listeners
+	// Start supporter listeners
 	for i := 0; i < SupporterCount; i++ {
 		go (func(i int, wg *sync.WaitGroup, t *godig.Table, c chan string, done chan bool) {
 			wg.Add(1)
