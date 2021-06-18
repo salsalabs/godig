7eecb7cce0297293e6262e530aa6b809beb6f948
Allen Leonard <aleonard@salsalabs.com>
2021-06-18 15:10:38 -0500

    Cannot close CSV object.

--- cmd/supporter_by_name/main.go
+++ cmd/supporter_by_name/main.go
@@ -56,7 +56,6 @@ func main() {
 		log.Fatalf("%s, %s", "names_and_emails.csv", err)
 	}
 	defer w.Flush()
-	defer w.Close()
 	var s []Supporter
 
 	// Each row is a string slice of (Last_Name, First_Name)
